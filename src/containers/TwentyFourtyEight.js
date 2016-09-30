71f79abeb2883844c0598153acd9f6a240205d6d
Nathan Bourke <nathanrbourke@gmail.com>
2016-09-30 15:05:01 -0400

    add react-hotkey, get keyboard events to work

--- src/containers/TwentyFourtyEight.js
+++ src/containers/TwentyFourtyEight.js
@@ -1,37 +1,89 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import hotkey from 'react-hotkey';
 
 import Cell from '../components/Cell';
 import Row from '../components/Row';
 
-const TwentyFourtyEight = function(props) {
-  // debugger;
-  const cells = props.board.map((row, rowIndex) => {
-    let rowComps = row.map((number, cellIndex) => {
-      return <Cell value={number}  key={cellIndex} />;
+
+hotkey.activate();
+class TwentyFourtyEight extends React.Component {
+  constructor(props) {
+    super(props)
+    this.hotkeyHandler = this.handleHotkey.bind(this);
+  }
+
+  handleHotkey(e) {
+    // debugger;
+    let { moveLeft, moveRight, moveUp, moveDown } = this.props
+    switch(e.key) {
+      case 'ArrowLeft':
+        moveLeft(); break;
+      case 'ArrowRight':
+        moveRight(); break;
+      case 'ArrowUp':
+        moveUp(); break;
+      case 'ArrowDown':
+        moveDown(); break;
+      default:
+        return;
+    }
+  }
+
+  componentDidMount() {
+    hotkey.addHandler(this.hotkeyHandler);
+  }
+
+  componentWillUnmount() {
+    hotkey.removeHandler(this.hotkeyHandler);
+  }
+
+  render() {
+    const cells = this.props.board.map((row, rowIndex) => {
+      let rowComps = row.map((number, cellIndex) => {
+        return <Cell value={number}  key={cellIndex} />;
+      });
+      return(
+        <Row key={rowIndex} >
+          {rowComps}
+        </ Row>
+      )
     });
 
     return(
-      <Row key={rowIndex}>
-        {rowComps}
-      </ Row>
-    )
-  });
-
-  return(
-    <div className="game">
-      <div className="board">
-        {cells}
+      <div className="game" >
+        <div className="board">
+          {cells}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 };
 
 const mapStateToProps = (state) => {
   return { board: state.board.board };
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    moveLeft: () => {
+      console.log("Left!")
+    },
+
+    moveRight: () => {
+      console.log("Right!")
+    },
+
+    moveUp: () => {
+      console.log("Up!")
+    },
+    moveDown: () => {
+      console.log("Down!")
+    }
+  }
+}
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(TwentyFourtyEight)
